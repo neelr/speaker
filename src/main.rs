6de13a49035fc419a49d9762f5d0a9233f4cb8c6
Neel Redkar <neel.redkar@gmail.com>
2021-01-28 16:44:47 -0800

    init

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+use rocket_contrib::json::Json;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct TextRes {
+    text: String
+}
+
+#[post("/speak", format="json", data="<value>")]
+fn speak(value: Json<TextRes>) -> String {
+    std::process::Command::new("say").args(&[format!("\"{}\"", &value.text)]).output().expect("erroring");
+    format!("hello")
+}
+
+fn main() {
+    println!("hello");
+    rocket::ignite().mount("/",routes![speak]).launch();
+}
